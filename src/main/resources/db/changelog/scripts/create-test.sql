3025e835dae49ccfa7cac4a79c68936ad0102d43
admin <rong@agile-solutions.ch>
2022-01-18 19:05:34 +0100

    liquibase

--- src/main/resources/db/changelog/scripts/create-test.sql
+++ src/main/resources/db/changelog/scripts/create-test.sql
@@ -2,15 +2,18 @@
 
 --changeset nvoxland:1
 
-create table test1 (
+create table testTable (
     id int primary key,
     name varchar(255)
 );
 --rollback drop table test1;
 
 --changeset nvoxland:2
-insert into test1 (id, name) values (1, ‘name 1′);
-insert into test1 (id,  name) values (2, ‘name 2′);
+insert into testTable (id, name) values (1, ‘name 1′);
+insert into testTable (id,  name) values (2, ‘name 2′);
 
 --changeset nvoxland:3 dbms:oracle
 create sequence seq_test;
+
+--rollback DROP TABLE
+--rollback testTable
