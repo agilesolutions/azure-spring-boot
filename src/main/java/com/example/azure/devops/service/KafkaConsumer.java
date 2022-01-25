25f5114d6f24ef4abc7c27a5facbe208f6b961fe
admin <rong@agile-solutions.ch>
2022-01-25 15:31:41 +0100

    liquibase

---
+++ src/main/java/com/example/azure/devops/service/KafkaConsumer.java
@@ -0,0 +1,34 @@
+package com.example.azure.devops.service;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.CountDownLatch;
+
+@Slf4j
+@Getter
+@Setter
+@Component
+public class KafkaConsumer {
+
+    private CountDownLatch latch = new CountDownLatch(1);
+    private String payload = null;
+
+    @KafkaListener(topics = "${test.topic}")
+    public void receive(ConsumerRecord<?, ?> consumerRecord) {
+        log.info("received payload='{}'", consumerRecord.toString());
+        setPayload(consumerRecord.toString());
+        latch.countDown();
+    }
+
+    public CountDownLatch getLatch() {
+        return latch;
+    }
+
+
+}
---
+++ src/test/java/com/example/azure/devops/service/EmbeddedKafkaIntegrationTest.java
@@ -0,0 +1,61 @@
+package com.example.azure.devops.service;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.test.EmbeddedKafkaBroker;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+//@DirtiesContext
+@EmbeddedKafka
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class EmbeddedKafkaIntegrationTest {
+
+    @Autowired
+    private EmbeddedKafkaBroker embeddedKafkaBroker;
+
+    @Autowired
+    public KafkaTemplate<String, String> template;
+
+    @Autowired
+    private KafkaConsumer consumer;
+
+    @Autowired
+    private KafkaProducer producer;
+
+    @Value("${test.topic}")
+    private String topic;
+
+    @Test
+    public void givenEmbeddedKafkaBroker_whenSendingtoDefaultTemplate_thenMessageReceived() throws Exception {
+        template.send(topic, "Sending with default template");
+        consumer.getLatch().await(10000, TimeUnit.MILLISECONDS);
+        assertThat(consumer.getLatch().getCount(), equalTo(0L));
+
+        assertThat(consumer.getPayload(), containsString("embedded-test-topic"));
+    }
+
+    @Test
+    public void givenEmbeddedKafkaBroker_whenSendingtoSimpleProducer_thenMessageReceived() throws Exception {
+        producer.send(topic, "Sending with our own simple KafkaProducer");
+        consumer.getLatch().await(10000, TimeUnit.MILLISECONDS);
+
+        assertThat(consumer.getLatch().getCount(), equalTo(0L));
+        assertThat(consumer.getPayload(), containsString("embedded-test-topic"));
+    }
+
+}
