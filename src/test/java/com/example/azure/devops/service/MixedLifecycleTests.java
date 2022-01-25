0d2800e6e3f2a81de67dfa7579d9fab6ec9fc836
admin <rong@agile-solutions.ch>
2022-01-25 17:01:36 +0100

    liquibase

---
+++ src/test/java/com/example/azure/devops/service/MixedLifecycleTests.java
@@ -0,0 +1,25 @@
+package com.example.azure.devops.service;
+
+import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@Testcontainers
+class MixedLifecycleTests {
+
+    // will be started before and stopped after each test method
+    @Container
+    private PostgreSQLContainer postgresqlContainer = new PostgreSQLContainer()
+            .withDatabaseName("foo")
+            .withUsername("foo")
+            .withPassword("secret");
+
+    @Test
+    void test() {
+        assertTrue(postgresqlContainer.isRunning());
+    }
+}
