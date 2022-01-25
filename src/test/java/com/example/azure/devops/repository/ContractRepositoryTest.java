bc8011c7ad71aaba8ce5fe235643fa1e19febfba
admin <rong@agile-solutions.ch>
2022-01-25 18:09:29 +0100

    liquibase

--- src/test/java/com/example/azure/devops/repository/ContractRepositoryTest.java
+++ src/test/java/com/example/azure/devops/repository/ContractRepositoryTest.java
@@ -5,6 +5,10 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.util.TestPropertyValues;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.testcontainers.containers.PostgreSQLContainer;
@@ -18,20 +22,34 @@ import static org.junit.jupiter.api.Assertions.*;
 @TestPropertySource(properties = {
         "spring.jpa.hibernate.ddl-auto=validate"
 })
+@ContextConfiguration(initializers = {ContractRepositoryTest.Initializer.class})
 class ContractRepositoryTest {
 
     @Autowired
     ContractRepository contractRepository;
 
     @Container
-    public static PostgreSQLContainer postgreSQLContainer = new PostgreSQLContainer()
-            .withPassword("inmemory")
-            .withUsername("inmemory");
+    public static PostgreSQLContainer postgreSQLContainer = new PostgreSQLContainer("postgres:11.1")
+            .withDatabaseName("integration-tests-db")
+            .withUsername("sa")
+            .withPassword("sa");
+
 
     @Test
     void findByNameNativeQuery() {
 
         contractRepository.save(Contract.builder().id(1L).name("test").build());
         assertThat(contractRepository.findByNameNativeQuery("test")).isNotNull();
+    }
+
+    static class Initializer
+            implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+        public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
+            TestPropertyValues.of(
+                    "spring.datasource.url=" + postgreSQLContainer.getJdbcUrl(),
+                    "spring.datasource.username=" + postgreSQLContainer.getUsername(),
+                    "spring.datasource.password=" + postgreSQLContainer.getPassword()
+            ).applyTo(configurableApplicationContext.getEnvironment());
+        }
     }
 }
