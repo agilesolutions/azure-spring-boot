31559bd816669d3ed5a8b05cc60e4de14aa51e6c
admin <rong@agile-solutions.ch>
2022-01-27 17:26:21 +0100

    liquibase

--- src/test/java/com/example/azure/devops/repository/KafkaTest.java
+++ src/test/java/com/example/azure/devops/repository/KafkaTest.java
@@ -1,7 +1,12 @@
 package com.example.azure.devops.repository;
 
+
+import com.example.azure.devops.service.KafkaConsumer;
+import com.example.azure.devops.service.KafkaProducer;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.testcontainers.containers.KafkaContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
@@ -9,8 +14,9 @@ import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(SpringExtension.class)
 @Testcontainers(disabledWithoutDocker = true)
@@ -19,6 +25,26 @@ public class KafkaTest {
     @Container
     public static KafkaContainer kafka = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:6.2.1"));
 
+
+    @Autowired
+    private KafkaConsumer consumer;
+
+    @Autowired
+    private KafkaProducer producer;
+
+    @Value("${test.topic}")
+    private String topic;
+
+
+    @Test
+    public void givenKafkaDockerContainer_whenSendingtoSimpleProducer_thenMessageReceived()
+            throws Exception {
+        producer.send(topic, "Sending with own controller");
+        consumer.getLatch().await(10000, TimeUnit.MILLISECONDS);
+
+        assertEquals(0L,consumer.getLatch().getCount());
+        assertTrue(consumer.getPayload().contains("embedded-test-topic"));
+    }
 
     @Test
     public void verifyRunning() {
