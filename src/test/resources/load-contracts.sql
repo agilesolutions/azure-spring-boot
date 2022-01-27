fea9f02bc2e4f6d051812675d6e52ca4fff6f57b
admin <rong@agile-solutions.ch>
2022-01-27 07:29:05 +0100

    liquibase

--- src/test/resources/load-contracts.sql
+++ src/test/resources/load-contracts.sql
@@ -1,3 +1,3 @@
-insert into contract values (2L, "contact2");
-insert into contract values (3L, "contact3");
-insert into contract values (4L, "contact4");
+insert into contract values (2, "contact2");
+insert into contract values (3, "contact3");
+insert into contract values (4, "contact4");
