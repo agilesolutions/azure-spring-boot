dbaa152e270ca09adafb0c96a38ca061a8b48705
admin <rong@agile-solutions.ch>
2022-01-27 08:31:07 +0100

    liquibase

--- src/test/java/com/example/azure/devops/repository/ContractRepositoryTest.java
+++ src/test/java/com/example/azure/devops/repository/ContractRepositoryTest.java
@@ -74,7 +74,7 @@ class ContractRepositoryTest {
         List<Contract> contracts = contractRepository.findAll();
 
         assertAll("test contracts"
-                , () -> assertEquals(1, contracts.size()));
+                , () -> assertEquals(2, contracts.size()));
     }
 
 
