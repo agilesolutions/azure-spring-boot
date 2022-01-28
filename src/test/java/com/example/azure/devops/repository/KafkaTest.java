10b4ae3b559dd02d5d0ff550b7c55fc8fb1dfae2
admin <rong@agile-solutions.ch>
2022-01-28 15:27:53 +0100

    liquibase

--- src/test/java/com/example/azure/devops/repository/KafkaTest.java
+++ src/test/java/com/example/azure/devops/repository/KafkaTest.java
@@ -50,18 +50,6 @@ public class KafkaTest {
     @Value("${test.topic:embedded-test-topic}")
     private String topic;
 
-
-    @Test
-    @Disabled
-    public void givenKafkaDockerContainer_whenSendingtoSimpleProducer_thenMessageReceived()
-            throws Exception {
-        producer.send(topic, Contract.builder().id(1).name("test").build());
-        consumer.getLatch().await(10000, TimeUnit.MILLISECONDS);
-
-        assertEquals(10L,consumer.getLatch().getCount());
-        //assertEquals("test",consumer.getPayload().getName());
-    }
-
     @Test
     public void verifyServers() {
 
@@ -80,6 +68,19 @@ public class KafkaTest {
         assertTrue(kafka.isRunning());
 
     }
+
+
+    @Test
+    //@Disabled
+    public void givenKafkaDockerContainer_whenSendingtoSimpleProducer_thenMessageReceived()
+            throws Exception {
+        producer.send(topic, Contract.builder().id(1).name("test").build());
+        consumer.getLatch().await(10000, TimeUnit.MILLISECONDS);
+
+        assertEquals(10L,consumer.getLatch().getCount());
+        //assertEquals("test",consumer.getPayload().getName());
+    }
+
 
     @TestConfiguration
     static class KafkaTestContainersConfiguration {
@@ -106,7 +107,7 @@ public class KafkaTest {
         @Bean
         public Map<String, Object> consumerConfigs() {
             Map<String, Object> props = new HashMap<>();
-            props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafka.getBootstrapServers());
+            props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "BROKER://localhost:9092");
             props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
             props.put(ConsumerConfig.GROUP_ID_CONFIG, "baeldung");
             props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
@@ -117,7 +118,7 @@ public class KafkaTest {
         @Bean
         public ProducerFactory<String, Contract> producerFactory() {
             Map<String, Object> configProps = new HashMap<>();
-            configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafka.getBootstrapServers());
+            configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "BROKER://localhost:9092");
             configProps.put(ConsumerConfig.GROUP_ID_CONFIG, "baeldung");
             configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
             configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
