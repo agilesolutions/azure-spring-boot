89e30694bf8ede62e71a164793d2fc160490938e
admin <rong@agile-solutions.ch>
2022-01-28 07:57:05 +0100

    liquibase

--- src/test/java/com/example/azure/devops/repository/KafkaTest.java
+++ src/test/java/com/example/azure/devops/repository/KafkaTest.java
@@ -3,10 +3,18 @@ package com.example.azure.devops.repository;
 
 import com.example.azure.devops.service.KafkaConsumer;
 import com.example.azure.devops.service.KafkaProducer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.*;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.testcontainers.containers.KafkaContainer;
@@ -15,13 +23,15 @@ import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(SpringExtension.class)
 @Testcontainers(disabledWithoutDocker = true)
-@ContextConfiguration(classes = {KafkaTestConfiguration.class})
+//@ContextConfiguration(classes = {KafkaTestConfiguration.class})
 public class KafkaTest {
 
     @Container
@@ -61,4 +71,47 @@ public class KafkaTest {
         assertNotEquals("",kafka.getBootstrapServers());
 
     }
+
+    @TestConfiguration
+    static class KafkaTestContainersConfiguration {
+
+        @Bean
+        ConcurrentKafkaListenerContainerFactory<Integer, String> kafkaListenerContainerFactory() {
+            ConcurrentKafkaListenerContainerFactory<Integer, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
+            factory.setConsumerFactory(consumerFactory());
+            return factory;
+        }
+
+        @Bean
+        public ConsumerFactory<Integer, String> consumerFactory() {
+            return new DefaultKafkaConsumerFactory<>(consumerConfigs());
+        }
+
+        @Bean
+        public Map<String, Object> consumerConfigs() {
+            Map<String, Object> props = new HashMap<>();
+            props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafka.getBootstrapServers());
+            props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+            props.put(ConsumerConfig.GROUP_ID_CONFIG, "baeldung");
+            props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+            props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+            return props;
+        }
+
+        @Bean
+        public ProducerFactory<String, String> producerFactory() {
+            Map<String, Object> configProps = new HashMap<>();
+            configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafka.getBootstrapServers());
+            configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+            configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+            return new DefaultKafkaProducerFactory<>(configProps);
+        }
+
+        @Bean
+        public KafkaTemplate<String, String> kafkaTemplate() {
+            return new KafkaTemplate<>(producerFactory());
+        }
+
+    }
+
 }
