4b4effe52b3acb789559afc291af502bbc074585
admin <rong@agile-solutions.ch>
2022-01-31 17:32:02 +0100

    liquibase

--- src/main/java/com/example/azure/devops/service/KafkaProducer.java
+++ src/main/java/com/example/azure/devops/service/KafkaProducer.java
@@ -5,7 +5,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Component;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
 
 @Slf4j
 @Component
@@ -17,7 +20,21 @@ public class KafkaProducer {
     public void send(String topic, Contract payload) {
         System.out.println(String.format("START sending payload %s to topc %s", payload, topic));
         log.info("sending payload='{}' to topic='{}'", payload, topic);
-        kafkaTemplate.send(topic, payload);
+        ListenableFuture<SendResult<String, Contract>> future = kafkaTemplate.send(topic, payload);
+
+        future.addCallback(new ListenableFutureCallback<SendResult<String, Contract>>() {
+
+            @Override
+            public void onSuccess(SendResult<String, Contract> result) {
+                System.out.println("Sent message=[" + payload.getName() +
+                        "] with offset=[" + result.getRecordMetadata().offset() + "]");
+            }
+            @Override
+            public void onFailure(Throwable ex) {
+                System.out.println("Unable to send message=["
+                        + payload.getName() + "] due to : " + ex.getMessage());
+            }
+        });
 
         System.out.println(String.format("FINISHED sending payload %s to topc %s", payload, topic));
     }
